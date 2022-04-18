7b2e18e7aaa2ddfa5d97badb95c05481646d9663
Shahnawaz Alam <shahnawaz.alam13@gmail.com>
2022-04-19 00:18:25 +1000

    Added new changes to notes-app

--- lesson-2-notes-app/app.js
+++ lesson-2-notes-app/app.js
@@ -1,5 +1,13 @@
+const validator = require('validator')
 const getNotes = require('./notes.js')
 
 const msg = getNotes()
 
 console.log(msg)
+
+// Check valid email
+console.log(validator.isEmail('shahnawaz')) // false
+console.log(validator.isEmail('shahnawaz@email.com')) // true
+// Check valid URL
+console.log(validator.isURL('./mead.io')) // false
+console.log(validator.isURL('https://mead.io')) // true
