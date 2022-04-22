4872bfdac4ba9659def3306e3f6c9cda97ac01c7
Shahnawaz Alam <shahnawaz.alam13@gmail.com>
2022-04-22 23:24:07 +1000

    Add parameter in notes app

--- lesson-2-notes-app/app.js
+++ lesson-2-notes-app/app.js
@@ -1,36 +1,7 @@
 const yargs = require('yargs');
-const chalk = require('chalk');
-// const validator = require('validator')
-// const getNotes = require('./notes.js')
+const notes = require('./notes.js')
 
-// const msg = getNotes()
 
-// console.log(msg)
-
-// const error = chalk.inverse.bold.red;
-// const success = chalk.bold.green;
-const greenArgs = chalk.bold.green;
-const yellowArgs = chalk.bold.yellow;
-
-// Check valid email
-// console.log(error(validator.isEmail('shahnawaz'))) // false
-// console.log(success(validator.isEmail('shahnawaz@email.com'))) // true
-// // Check valid URL
-// console.log(error(validator.isURL('./mead.io'))) // false
-// console.log(success(validator.isURL('https://mead.io'))) // true
-// console.log(success('Success!'))
-
-// // Manually access args
-// let arg = process.argv[2]
-// if (arg && arg !== '') {
-//     console.log('Argument input: ' + consoleArgs(arg))
-// }
-
-// compare process.argv and yargs
-// console.log(process.argv)
-// console.log(yargs.argv)
-
-// Customise yargs version
 yargs.version('1.1.0')
 
 // Create add command
@@ -50,8 +21,7 @@ yargs.command({
         }
     },
     handler: function (argv) {
-        console.log(yellowArgs('Title: ') + greenArgs(argv.title))
-        console.log(yellowArgs('Body: ') + greenArgs(argv.body))
+        notes.addNote(argv.title, argv.body)
     }
 })
 
--- lesson-2-notes-app/notes.js
+++ lesson-2-notes-app/notes.js
@@ -1,5 +1,47 @@
+const fs = require('fs')
+
 const getNotes = function () {
     return 'Your notes...'
 }
 
-module.exports = getNotes
+const addNote = function (title, body) {
+    const notes = loadNotes()
+
+    // implementing an array filter
+    const duplicateNotes = notes.filter(function (note) {
+        return note.title == title
+    })
+
+    if (duplicateNotes.length == 0) {
+        notes.push({
+            title: title,
+            body: body
+        })
+
+        saveNotes(notes)
+        console.log('New note added!')
+    } else {
+        console.log('Note title taken!')
+    }
+}
+
+const saveNotes = function (notes) {
+    const dataJSON = JSON.stringify(notes)
+    fs.writeFileSync('notes.json', dataJSON)
+    console.log('saving to notes.json')
+}
+
+const loadNotes = function () {
+    try {
+        const dataBuffer = fs.readFileSync('notes.json')
+        const dataJSON = dataBuffer.toString()
+        return JSON.parse(dataJSON)
+    } catch (e) {
+        return []
+    }
+}
+
+module.exports = { 
+    getNotes: getNotes,
+    addNote: addNote
+}
