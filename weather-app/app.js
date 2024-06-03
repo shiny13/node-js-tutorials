e55a10b6c4eae915acca150bee8daaa4ccbbdb5a
shiny13 <shahnawaz.alam13@gmail.com>
2024-06-03 23:23:56 +1000

    using command line args to send request for weather app

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,18 +1,25 @@
 const forecast = require('./utils/forecast')
 const geocode = require('./utils/geocode')
 
-geocode('Philadelphia', (error, data) => {
-    console.log('Error', error)
-    console.log('Data', data)
+const address = process.argv[2]
 
-    forecast(data.latitude, data.longitude, (error, data) => {
-        console.log('Error', error)
+if (!address) {
+    console.log('Please provide an address')
+} else {
+    geocode(address, (error, data) => {
+        if (error) {
+            return console.log('Error', error)
+        }
+        
         console.log('Data', data)
+        forecast(data.latitude, data.longitude, (error, forecastData) => {
+            if (error) {
+                return console.log('Error', error)
+            }
+            
+            console.log(data.location)
+            console.log(forecastData)
+        })
+        
     })
-    
-})
-
-// forecast(-75.7088, 44.1545, (error, data) => {
-//     console.log('Error', error)
-//     console.log('Data', data)
-// })
+}
