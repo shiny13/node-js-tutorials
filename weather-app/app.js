1158e8fa5208b9eccad091b2ae042948cac5c39a
shiny13 <shahnawaz.alam13@gmail.com>
2024-06-03 23:48:51 +1000

    refactored the code using object destructuring

--- weather-app/app.js
+++ weather-app/app.js
@@ -6,18 +6,18 @@ const address = process.argv[2]
 if (!address) {
     console.log('Please provide an address')
 } else {
-    geocode(address, (error, data) => {
+    geocode(address, (error, { latitude, longitude, location } = {}) => {
         if (error) {
             return console.log('Error', error)
         }
         
         console.log('Data', data)
-        forecast(data.latitude, data.longitude, (error, forecastData) => {
+        forecast(data, (error, forecastData) => {
             if (error) {
                 return console.log('Error', error)
             }
             
-            console.log(data.location)
+            console.log(location)
             console.log(forecastData)
         })
         
--- weather-app/utils/forecast.js
+++ weather-app/utils/forecast.js
@@ -2,13 +2,13 @@ const request = require('postman-request')
 
 const forecast = (latitude, longitude, callback) => {
     const url = 'https://api.weatherstack.com/current?access_key=f27525cb58077d1151f33f61b2576dc5&query=' + latitude + ',' + longitude + '&units=m'
-    request({ url: url, json: true }, (error, response) => {
+    request({ url: url, json: true }, (error, { body }) => {
         if (error) {
             callback('Unable to connect to the weather service.', undefinded)
-        } else if (response.body.error) {
+        } else if (body.error) {
             callback('Unable to find location.', undefinded)
         } else {
-            callback(undefinded, 'It is currently ' + response.body.current.temperature + ' degrees out. It feels like ' + response.body.current.feelslike + ' degrees out.')
+            callback(undefinded, 'It is currently ' + body.current.temperature + ' degrees out. It feels like ' + body.current.feelslike + ' degrees out.')
         }
     })
 }
