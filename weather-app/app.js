febd32bac273fe9b776a9b967757142bac496b1f
shiny13 <shahnawaz.alam13@gmail.com>
2024-06-03 23:18:23 +1000

    callback chaining to make one request, use result json to make second request

--- weather-app/app.js
+++ weather-app/app.js
@@ -4,9 +4,15 @@ const geocode = require('./utils/geocode')
 geocode('Philadelphia', (error, data) => {
     console.log('Error', error)
     console.log('Data', data)
+
+    forecast(data.latitude, data.longitude, (error, data) => {
+        console.log('Error', error)
+        console.log('Data', data)
+    })
+    
 })
 
-forecast(-75.7088, 44.1545, (error, data) => {
-    console.log('Error', error)
-    console.log('Data', data)
-})
+// forecast(-75.7088, 44.1545, (error, data) => {
+//     console.log('Error', error)
+//     console.log('Data', data)
+// })
